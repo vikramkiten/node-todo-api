76a7b9948dbd75ea400789c97277db6ba64cb732
Vikram Bezawada <bezawada.battivikramarka@gmail.com>
2018-06-12 19:57:52 +0530

    Created server file which is the launcher of the application

---
+++ server/models/todo.js
@@ -0,0 +1,19 @@
+let mongoose=require('mongoose');
+let Todo=mongoose.model('Todo',{
+    text:{
+        type:String,
+        minlength:1,
+        trim:true,
+        required:true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
+    completedAt:{
+        type:Number,
+        default:null
+    }
+});
+
+module.exports={Todo};
---
+++ server/server.js
@@ -0,0 +1,25 @@
+let express=require('express');
+let bodyparser=require('body-parser');
+
+const {mongoose}=require('./db/mongoose');
+
+let {Todo}=require('./models/todo');
+let {User}=require('./models/user');
+let app=express();
+app.use(bodyparser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo=new Todo({
+      text:req.body.text
+  });
+  todo.save().then((doc)=>{
+      res.send(doc);
+    
+},(err)=>{
+    
+    res.status(status).send(err);
+});
+});
+app.listen(3000,()=>{
+    console.log("You are good to go, mate!");
+})
