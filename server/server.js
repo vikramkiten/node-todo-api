458dd2fa4f21c405f2ce494773daffab3c478fc8
Vikram Bezawada <bezawada.battivikramarka@gmail.com>
2018-06-13 17:54:45 +0530

    Add route to get info through the ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 let express=require('express');
 let bodyparser=require('body-parser');
-
+let {ObjectID}=require('mongodb')
 const {mongoose}=require('./db/mongoose');
 
 let {Todo}=require('./models/todo');
@@ -30,6 +30,23 @@ app.get('/todos',(req,res)=>{
        res.status(400).send(e);
     })
 })
+
+app.get('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo)
+        {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
 app.listen(3000,()=>{
     console.log("You are good to go, mate!");
 })
