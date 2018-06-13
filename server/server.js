33208f2ef3be173104e7bdfcf4c03dd670960562
Vikram Bezawada <bezawada.battivikramarka@gmail.com>
2018-06-13 15:44:06 +0530

    Added a new route to server file

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.post('/todos',(req,res)=>{
     res.status(status).send(err);
 });
 });
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+    res.send({
+        todos
+    });
+    },(e)=>{
+       res.status(400).send(e);
+    })
+})
 app.listen(3000,()=>{
     console.log("You are good to go, mate!");
 })
